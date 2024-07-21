698dc48f04c39c43d429a9d36fba274fc24e0a85
Yutong Deng <yutong.deng09@gmail.com>
2024-07-21 11:22:03 +0300

    added ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -1,7 +1,20 @@
+
 void ft_sort_int_tab(int *tab, int size)
 {
-	
-
-
-
+	int	i;
+	int	j;
+	j = 0;
+	i = 0;
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			j = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = j;
+			i = 0;
+		}
+		else
+			i++;
+	}
 }
